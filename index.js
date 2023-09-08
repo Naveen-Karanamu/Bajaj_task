27816455f0bda7c0d20fb757c9f7bd3f80ca7b19
Naveen Kumar <karanamunaveen2002@gmail.com>
2023-09-08 16:00:15 +0530

    POST API v1

--- index.js
+++ index.js
@@ -41,9 +41,29 @@ app.get('/bfhl', (req, res) => {
   // POST endpoint
   app.post('/bfhl', (req, res) => {
     // Check if there is data in req.body
-    if (req.body && Object.keys(req.body).length > 0) {
-      // Data found in req.body
-      res.send('This is a POST request with data.');
+  if (req.body && Array.isArray(req.body.data) && req.body.data.length > 0) {
+    const inputData = req.body.data;
+    
+    // Extract numbers and alphabets from the input data
+    const numbers = inputData.filter(item => !isNaN(item));
+    const alphabets = inputData.filter(item => isNaN(item) && item.length === 1 && /[a-zA-Z]/.test(item));
+
+    // Find the highest alphabet (case insensitive)
+    const highest_alphabet = alphabets.reduce((highest, current) => {
+      return current.toLowerCase() > highest.toLowerCase() ? current : highest;
+    }, 'A');
+
+    const response = {
+      is_success: true,
+      user_id: 'john_doe_17091999',
+      email: 'john@xyz.com',
+      roll_number: 'ABCD123',
+      numbers: numbers,
+      alphabets: alphabets,
+      highest_alphabet: [highest_alphabet],
+    };
+
+    res.json(response);
     } else {
       // No data in req.body
       res.json({Error: "Data Not Found"})
